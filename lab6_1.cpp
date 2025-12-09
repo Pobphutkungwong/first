20052de0959596618ae29556f90f35a07d15e4a5
Pobphut <phopphut@gmail.com>
2025-12-09 16:39:18 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,11 @@
 using namespace std;
 
 int main(){
+    int i = 0;
     cout << "Enter an integer: ";
+    cin << i;
     cout << "#Even numbers = ";
     cout << "#Odd numbers = ";
+
     return 0;
 }
