c06ceea5d528bc2652c5f08af20d8508c7353140
Pobphut <phopphut@gmail.com>
2025-12-09 17:15:28 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,11 +2,24 @@
 using namespace std;
 
 int main(){
-    int i = 0;
+    int num = -1;
+    int E = 0;
+    int O = 0;
+    
+    while(num != 0){
     cout << "Enter an integer: ";
-    cin << i;
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> num;
+    
+    if(num > 0 && num % 2 == 0){
+        E = E + 1;
+    }
+    else if(num % 2 == 1){
+        O = O + 1;
+    }
+    
+    }
 
+    cout << "#Even numbers = " << E << "\n";
+    cout << "#Odd numbers = " << O;
     return 0;
 }
